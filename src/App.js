283e0a985e387d72efc46b40133e8bcb6ab8331b
carolinabaam <carolina@formkitchens.com>
2021-05-04 10:32:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import FeaturedCompanies from './components/FeaturedCompanies';
+import Hero from './components/Hero';
+import JobOffers from './components/JobOffers';
+import GlobalStyle from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Hero />
+      <JobOffers />
+      <FeaturedCompanies />
+    </>
   );
 }
 
